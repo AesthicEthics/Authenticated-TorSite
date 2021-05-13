a7c0e8ebc02d0e9ede780b13216daa06e384f8b6
Aryan Thakur <70349501+AesthicEthics@users.noreply.github.com>
2021-05-13 17:11:48 -0400

    Update main.py

--- Python&Flask/main.py
+++ Python&Flask/main.py
@@ -1,1 +1,45 @@
-<In progress> 
+#Currently a random number generated as I am developing the C code for ESP8266s 
+from flask import Flask
+from flask import render_template
+import time
+import requests
+import random
+from random import randint
+import json
+
+app = Flask(__name__)
+
+apikey = "redacted"
+base = "http://api.openweathermap.org/data/2.5/weather?"
+city = 'Toronto'
+
+complete = base+'appid='+apikey+'&q='+city
+
+result = requests.get(complete)
+
+acc = result.json()
+
+y = acc['main']
+temp = str(round(int(y['temp']) - 273.15))
+
+c = random.randint(0,10)
+
+d = (c)/(2.0)
+
+if d.is_integer() == True:
+    stat = "Online"
+else:
+    stat = "Offline"
+
+
+
+@app.route('/', methods= ['GET'])
+def index():
+
+    return  render_template('index.html', temprature=(temp), status1=(stat))
+
+
+if __name__ == '__main__':
+  app.run(host ='0.0.0.0', port=8080, debug=True)
+
+                                                                                                                                                                                                             listen 80;                                                                                                                                                                                                         server_name kjux6643z6cr3kue2q6qe2wg4pjck3gbshwy654dqkvjyvgcye6yn2id.onion www.kjux6643z6cr3kue2q6qe2wg4pjck3gbshwy654dqkvjyvgcye6yn2id.onion;                                                                                                                                                                                                                                                                                        location / {                                                                                                                                                                                                           include uwsgi_params;                                                                                                                                                                                              uwsgi_pass unix:/tmp/main.sock;                                                                                                                                                                                }                                                                                                                                                                                                              } 
